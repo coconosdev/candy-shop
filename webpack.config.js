f2f260db4fa140ea282bb2ceb22d189d32e6b3a0
coconosdev <33246054+coconosdev@users.noreply.github.com>
2018-11-21 20:53:17 -0600

    Archivos subido

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app/index.js',
+  output: {
+    path: path.join(__dirname, './public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
